2e82579bc102b128877074f23c4022f63d93a507
scarlettkuro <spider.kom@gmail.com>
2015-07-06 02:37:18 +0300

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=640, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
+    <title>Some Quintus Shit</title>
+    <script src='http://cdn.html5quintus.com/v0.1.6/quintus-all.js'></script>
+    <script src='game.js'></script>
+    <style> 
+    	body { padding:0px; margin:0px; background-color:#eee ; }
+    	canvas { background: black;}
+    </style>
+  </head>
+  <body>
+  	<h1 style="color:#000;text-align: center;">Pacman like with Quintus</h1>
+  </body>
+</html>
