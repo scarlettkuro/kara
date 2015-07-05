d466609fdf2cf1dd7aaa65d27c6e7f5a6f599401
scarlettkuro <spider.kom@gmail.com>
2015-07-06 02:56:59 +0300

    test dropbox host

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=640, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
     <title>Kara Roguelike</title>
     <script src='http://cdn.html5quintus.com/v0.1.6/quintus-all.js'></script>
-    <script src='game.js'></script>
+    <script src='https://www.dropbox.com/s/i4rzyfux3073kir/game.js?dl=0'></script>
     <style> 
     	body { padding:0px; margin:0px; background-color:#abc ; }
     	canvas { background: black;}
