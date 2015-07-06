c91db4910b11c1c487ba26dd665e943607926542
scarlettkuro <spider.kom@gmail.com>
2015-07-06 03:19:32 +0300

    *

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=640, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
     <title>Kara Roguelike</title>
-    <script src='http://cdn.html5quintus.com/v0.1.6/quintus-all.js'></script>
-    <script src='game.js'></script>
+    
     <style> 
     	body { padding:0px; margin:0px; background-color:#abc ; }
     	canvas { background: black;}
     </style>
   </head>
   <body>
-  	
+  	<script src='http://cdn.html5quintus.com/v0.1.6/quintus-all.js'></script>
+    <script src='game.js'></script>
   </body>
 </html>
