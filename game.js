fea6e54c3605bccaf7e29df1624597e56ecdd5bc
scarlettkuro <spider.kom@gmail.com>
2015-07-06 03:12:57 +0300

    hello world scene popup

--- game.js
+++ game.js
@@ -1,4 +1,19 @@
 window.addEventListener("load",function() {
+//--application start
 var Q = Quintus().include("Sprites, Scenes, Input").setup({ maximize: true });
 
+
+Q.scene('helloworld',function(stage) {
+    var container = stage.insert(new Q.UI.Container({
+      x: Q.width/2, y: Q.height/2, fill: "rgba(0,0,0,0.5)"
+    }));
+    
+    var button = container.insert(new Q.UI.Button({ x: 0, y: 0, fill: "#CCCCCC",
+                                             label: "Well, we've started" }));  
+
+        container.fit(20);
+  });
+  
+Q.stageScene("helloworld");
+//--application end
 });
